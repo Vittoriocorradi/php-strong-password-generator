346028432f0592d88ca9693382bb43f1edee0851
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-04-11 16:55:34 +0200

    Dati caratteri casuali

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+
+$lowercase_characters = range('a','z');
+$uppercase_characters = range('A', 'Z');
+$number_characters = range('0', '9');
+$special_characters = ['~','!','@','#','$','*','%','`','?','[',']','{','}',';','<','>','?','.',',','_','-','(',')'];
+
+$random_characters = array_merge($lowercase_characters, $uppercase_characters, $number_characters, $special_characters);
+    
+var_dump($random_characters);
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
